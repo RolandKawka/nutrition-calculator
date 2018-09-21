39b93b3c261cd1d8947a6b51b78de170f12b3d7b
Roland Kawka <roland.kawka@netins.it>
2018-09-21 20:27:54 +0200

    add sequelize

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const Sequelize = require('sequelize');
 const graphqlHTTP = require('express-graphql');
-const knex = require('knex');
 const schema = require('./schema/schema');
 
 const app = express();
 
-const db = knex({
-    client: 'pg',
-    connection: process.env.POSTGRES_URI,
-});
+const sequelize = new Sequelize(process.env.POSTGRES_URI);
 
 app.use(cors());
 app.use(morgan('combined'));
@@ -20,7 +17,13 @@ app.use('/graphql', graphqlHTTP({
     graphiql: true,
 }));
 
-db('hello')
-    .then(res => console.log(res));
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch((err) => {
+        console.error('Unable to connect to the database:', err);
+    });
 
 app.listen(3000, () => console.log('app is running on port 3000'));
